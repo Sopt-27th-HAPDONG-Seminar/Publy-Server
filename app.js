280fe08ca72c6619659cc5bb7a65842ce86576a8
sophryu99 <sehwa_ryu@berkeley.edu>
2020-12-06 22:26:08 +0900

    Apply Sequelize: databse table created

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 const { sequelize } = require('./models');
 
 /** alter: true를 해줘야 데이터베이스에 수정이 반영됨 */
-sequelize.sync({ alter: true })
+sequelize.sync({ alter: false })
 .then(() => {
  console.log('데이터베이스 연결 성공.');
 })
